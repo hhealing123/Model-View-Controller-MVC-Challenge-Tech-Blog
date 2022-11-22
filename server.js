92181c161df47c60f3893150d0cfd054ae8cc925
Harvey Lee <hhealing123@gmail.com>
2022-11-21 18:12:42 -0600

    Revised Procfile and server

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const hbs = exphbs.create({ helpers });
 
 // Set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
